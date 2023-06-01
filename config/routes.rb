72e182b10481d2f3ecbfdaafc2e5cd2806869d22
Hammam Firdaus <hammamxalf@gmail.com>
2023-06-02 05:40:52 +0700

    feat: scaffold polling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # polling's
+  resources :pollings
+
+  # dashboard index
   get 'dashboards', to: 'dashboard#index'
+
+  # devise
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_134141) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_222532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+
+  create_table "pollings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "title"
+    t.datetime "duration", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pollings_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_134141) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pollings", "users"
 end

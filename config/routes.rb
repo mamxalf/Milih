34b351b46898fc2b8ee4adfde6bac0a951e224f5
Hammam Firdaus <hammamxalf@gmail.com>
2023-06-02 05:14:36 +0700

    feat: dashboard meraki ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'dashboards', to: 'dashboard#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }

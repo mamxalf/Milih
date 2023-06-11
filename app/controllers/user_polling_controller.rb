ac7b9cd7cfb32bd69c443b26d99594bcd0189ce0
Hammam Firdaus <hammamxalf@gmail.com>
2023-06-11 08:44:32 +0700

    feat: sent data amount polling answer notif

--- app/controllers/user_polling_controller.rb
+++ app/controllers/user_polling_controller.rb
@@ -5,6 +5,7 @@ class UserPollingController < ApplicationController
     code = params[:code]
     @polling = Polling.find_by(code:)
     if code.nil? || @polling.nil? || REDIS_CONN.get("#{request.remote_ip}::#{@polling.id}").eql?('true')
+      # if code.nil? || @polling.nil?
       respond_to do |format|
         format.html { redirect_to user_status_pollings_url, alert: 'Polling not found!' }
       end
@@ -17,12 +18,15 @@ class UserPollingController < ApplicationController
     answer_id = params['answer']
     polling_answer = PollingAnswer.find_by(id: answer_id)
     if polling_answer.nil? || !REDIS_CONN.set("#{request.remote_ip}::#{polling_answer.polling_id}", true, nx: true, ex: 1.day)
+      # if polling_answer.nil?
       respond_to do |format|
         format.html { redirect_to user_status_pollings_url, alert: 'Polling not found!' }
       end
     else
       REDIS_CONN.incr(answer_id)
       if polling_answer.update(amount: REDIS_CONN.get(answer_id))
+        polling_id = polling_answer.polling_id
+        send_broadcast_action_cable!(polling_id, get_all_amount(polling_id))
         respond_to do |format|
           format.html { redirect_to user_status_pollings_url, notice: 'Thank you for choosing polling.' }
         end
@@ -38,5 +42,18 @@ class UserPollingController < ApplicationController
     respond_to do |format|
       format.html { render 'user_polling/status' }
     end
+  end
+
+  private
+
+  def get_all_amount(polling_id)
+    answer_polling_amount = Polling.find(polling_id).polling_answers.pluck(:id).map do |polling_answer_id|
+      { polling_answer_id:, amount: REDIS_CONN.get(polling_answer_id).to_i }
+    end
+    { total: answer_polling_amount.pluck(:amount).sum, data: answer_polling_amount }
+  end
+
+  def send_broadcast_action_cable!(polling_id, data)
+    ActionCable.server.broadcast("pollings_#{polling_id}", data)
   end
 end

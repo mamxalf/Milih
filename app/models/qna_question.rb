fc8e3ef94fcf3fb03a35dfdc87331c466b3da5fb
Hammam Firdaus <hammamxalf@gmail.com>
2023-06-14 10:27:15 +0700

    feat: first step turbo question

--- app/models/qna_question.rb
+++ app/models/qna_question.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class QnaQuestion < ApplicationRecord
+  after_create_commit { broadcast_prepend_to qna_room_id, locals: { room: qna_room, qna: self } }
+
   belongs_to :qna_room
 end

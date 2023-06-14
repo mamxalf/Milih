c4f5fdde743c9639e64a62432b95797fccd1cc78
Hammam Firdaus <hammamxalf@gmail.com>
2023-06-14 09:17:49 +0700

    feat: form QnA Room

--- app/controllers/qna_questions_controller.rb
+++ app/controllers/qna_questions_controller.rb
@@ -3,13 +3,9 @@
 class QnaQuestionsController < ApplicationController
   def form
     code = params[:code]
-    @qna_room = QnaRoom.find_by(code:)
-    if code.nil? || @qna_room.nil?
-      respond_to do |format|
-        format.html { redirect_to user_status_pollings_url, alert: 'QnA not found!' }
-      end
-    else
-      render 'qna_questions/form'
+    @room = QnaRoom.find_by(code:)
+    respond_to do |format|
+      format.html { render 'qna_questions/form' }
     end
   end
 
